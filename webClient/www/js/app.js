98dad1f4c5f72e3315271caf45d509745a9fded5
yujuncai <yujuncai@126.com>
2023-02-17 15:58:30 +0800

    feat[webclient]: 新增H5客户端

--- webClient/www/js/app.js
+++ webClient/www/js/app.js
@@ -174,7 +174,7 @@ $$(document).on('page:init', '.page[data-name="contact"]', function (e) {
     }
   });
 });
-<<<<<<< HEAD
+
 
 
 $$(document).on('page:init', '.page[data-name="profile"]', function (e) {
@@ -182,5 +182,4 @@ $$(document).on('page:init', '.page[data-name="profile"]', function (e) {
  initprofile();
 });
 
-=======
->>>>>>> origin/main
+
