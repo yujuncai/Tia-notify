55935053955a04a1576194a89565992609333726
yujuncai <yujuncai@126.com>
2023-02-20 14:30:59 +0800

    feat[client]: 模拟50个用户登陆

--- client/src/main/java/org/kikyou/tia/netty/client/shell/MyCommands.java
+++ client/src/main/java/org/kikyou/tia/netty/client/shell/MyCommands.java
@@ -2,17 +2,24 @@ package org.kikyou.tia.netty.client.shell;
 
 import cn.hutool.json.JSONUtil;
 import io.socket.client.Ack;
+import io.socket.client.IO;
+import io.socket.client.Manager;
 import io.socket.client.Socket;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.kikyou.tia.netty.chatroom.constant.EventNam;
 import org.kikyou.tia.netty.chatroom.models.User;
+import org.kikyou.tia.netty.chatroom.utils.MySecureUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.util.StringUtils;
 
+import java.net.URI;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @ShellComponent
 @RequiredArgsConstructor
@@ -57,32 +64,83 @@ public class MyCommands {
         socket.emit(EventNam.REGISTER, JSONUtil.parseObj(user));
     }
 
+
+
+
+    ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(60);
+    @SneakyThrows
     @ShellMethod(value = "register50")
     public void register50(){
         for (int i = 0; i < 50; i++) {
-            System.out.println("发起register");
-            User user = new User();
-            user.setName(String.valueOf(i));
-            user.setPassword(String.valueOf(i));
-            user.setAvatarUrl("img/20180414165827.jpg");
-            user.setTime(1L);
-            user.setIp("127.0.0.1");
-            socket.emit(EventNam.REGISTER, JSONUtil.parseObj(user));
+
+            int finalI = i;
+            Runnable runnable = new Runnable(){
+                @SneakyThrows
+                @Override
+                public void run() {
+                    try {
+                        IO.Options options = new IO.Options();
+                        String signature = MySecureUtil.aesEncrypt("jvZJhHtp3vOVmpool6QlMw==", "/tia-java");
+                        String s="http://localhost".concat("?").concat("appid=987654321").concat("&").concat("signature="+signature);
+                        Manager m=new Manager(new URI(s));
+                        Socket  socket=m.socket("/tia-java");
+                        Socket connect = socket.connect();
+                        System.out.println("发起register");
+                        User user = new User();
+                        user.setName(String.valueOf(finalI));
+                        user.setPassword(String.valueOf(finalI));
+                        user.setAvatarUrl("img/20180414165827.jpg");
+                        user.setTime(1L);
+                        user.setIp("127.0.0.1");
+                        connect.emit(EventNam.REGISTER, JSONUtil.parseObj(user));
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                }
+            };
+            // 将任务交给线程池管理
+            newFixedThreadPool.execute(runnable);
         }
 
     }
+    //模拟50个用户登陆
+    @SneakyThrows
     @ShellMethod(value = "login50")
     public void login50() {
 
         for (int i = 0; i < 50; i++) {
-            System.out.println("发起login");
-            User user = new User();
-            user.setName(String.valueOf(i));
-            user.setPassword(String.valueOf(i));
-            user.setAvatarUrl("img/20180414165827.jpg");
-            user.setTime(1L);
-            user.setIp("127.0.0.1");
-            socket.emit(EventNam.LOGIN, JSONUtil.parseObj(user));
+
+            int finalI = i;
+            Runnable runnable = new Runnable(){
+                @SneakyThrows
+                @Override
+                public void run() {
+                    try {
+                        IO.Options options = new IO.Options();
+                        String signature = MySecureUtil.aesEncrypt("jvZJhHtp3vOVmpool6QlMw==", "/tia-java");
+                        String s="http://localhost".concat("?").concat("appid=987654321").concat("&").concat("signature="+signature);
+                        Manager m=new Manager(new URI(s));
+                        Socket  socket=m.socket("/tia-java");
+                        Socket connect = socket.connect();
+                        System.out.println("发起login");
+                        User user = new User();
+                        user.setName(String.valueOf(finalI));
+                        user.setPassword(String.valueOf(finalI));
+                        user.setAvatarUrl("img/20180414165827.jpg");
+                        user.setTime(1L);
+                        user.setIp("127.0.0.1");
+                        connect.emit(EventNam.LOGIN, JSONUtil.parseObj(user));
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                }
+            };
+            // 将任务交给线程池管理
+            newFixedThreadPool.execute(runnable);
         }
     }
 
