d2b73cbaf594f882639dae5459752dddc8f863a4
yujuncai <yujuncai@126.com>
2023-11-01 14:50:59 +0800

    perf[service]: format code

--- server/src/main/java/org/kikyou/tia/netty/notify/handler/monitor/ProtoBufHandler.java
+++ server/src/main/java/org/kikyou/tia/netty/notify/handler/monitor/ProtoBufHandler.java
@@ -1,6 +1,5 @@
 package org.kikyou.tia.netty.notify.handler.monitor;
 
-import cn.hutool.json.JSONUtil;
 import com.baidu.bjf.remoting.protobuf.Codec;
 import com.baidu.bjf.remoting.protobuf.ProtobufProxy;
 import com.corundumstudio.socketio.AckRequest;
@@ -9,16 +8,8 @@ import com.corundumstudio.socketio.annotation.OnEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kikyou.tia.netty.notify.constant.EventNam;
-import org.kikyou.tia.netty.notify.constant.SystemType;
 import org.kikyou.tia.netty.notify.models.protobuf.ProtobufMessage;
-import org.kikyou.tia.netty.notify.web.dto.InfoDto;
-import org.kikyou.tia.netty.notify.web.dto.UptimeDto;
-import org.kikyou.tia.netty.notify.web.dto.UsageDto;
-import org.kikyou.tia.netty.notify.web.service.InfoService;
-import org.kikyou.tia.netty.notify.web.service.UptimeService;
-import org.kikyou.tia.netty.notify.web.service.UsageService;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 
 /**
@@ -38,7 +29,6 @@ public class ProtoBufHandler {
 
 
         log.info("PROTOBUF data {}", data);
-
         Codec<ProtobufMessage> simpleTypeCodec = ProtobufProxy.create(ProtobufMessage.class);
         ProtobufMessage decode = simpleTypeCodec.decode(data);
         log.info("PROTOBUF decode {}", decode.toString());
