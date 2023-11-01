293eba7256334116bdcd1890c86c49ae2ed41afc
yujuncai <yujuncai@126.com>
2023-11-01 11:42:45 +0800

    fix[service]: exclude Redisson  RedissonAutoConfigurationV2

--- client/src/main/java/org/kikyou/tia/netty/client/ShellApplication.java
+++ client/src/main/java/org/kikyou/tia/netty/client/ShellApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
 
-@SpringBootApplication(exclude = { RedissonAutoConfigurationV2.class})
+@SpringBootApplication(exclude = {RedissonAutoConfigurationV2.class})
 public class ShellApplication {
 
     public static void main(String[] args) {
