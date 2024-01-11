77891550c72a4f5088f751e2586521266dd2a2c0
yujuncai <yujuncai@126.com>
2024-01-11 16:32:51 +0800

    feture[sever]: 统一获取bean Name

---
+++ server/src/main/java/org/kikyou/tia/netty/notify/annotation/RegisterToListener.java
@@ -0,0 +1,16 @@
+package org.kikyou.tia.netty.notify.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RegisterToListener {
+
+    Boolean opened = true;
+
+}
+
+
