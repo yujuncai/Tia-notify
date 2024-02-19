0f32c20f3a14a2f08fe5df11f24b768c324d4089
yujuncai <yujuncai@126.com>
2024-02-19 09:26:39 +0800

    update[sever]: update

--- server/src/main/java/org/kikyou/tia/netty/notify/annotation/RegisterToListener.java
+++ server/src/main/java/org/kikyou/tia/netty/notify/annotation/RegisterToListener.java
@@ -4,7 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+//用于对多个namespace注册listenner的扫描
 @Target({ ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RegisterToListener {
